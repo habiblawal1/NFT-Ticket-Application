56809e9c0c7e544b35a8679cddcb4578cb95678e
habiblawal1 <habiblawal@hotmail.co.uk>
2022-05-16 23:13:55 +0100

    Added new contract

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
 // export const nftaddress = "0x55bEE48806F2c935B5Bc6e364d1082d85E13cdE8";
 // export const nftmarketaddress = "0x26C7cb8d07D92C742b310C02716E726823C1Dc47";
 
-export const nftmarketaddress = "0x7a8436A983707bA99813d21d0C7d0d035D5eaD21";
-export const nftaddress = "0x5Dde79546585866d45f73405B942cb0a2FB9F1D4";
+// export const nftmarketaddress = "0x7a8436A983707bA99813d21d0C7d0d035D5eaD21";
+// export const nftaddress = "0x5Dde79546585866d45f73405B942cb0a2FB9F1D4";
 
 // export const nftmarketaddress = "0xC67343CAffd07388274cc40a9a344212b2287E4a";
 // export const nftaddress = "0xd084dd18E0555544a3B85513c3E3f1C87A6c958B";
@@ -14,6 +14,11 @@ export const nftaddress = "0x5Dde79546585866d45f73405B942cb0a2FB9F1D4";
 // TicketMarket Contract deployed to: 0xC67343CAffd07388274cc40a9a344212b2287E4a
 // NFTTicket Contract deployed to: 0xd084dd18E0555544a3B85513c3E3f1C87A6c958B
 
+export const nftmarketaddress = "0xD542F8f9fc40d4038AEab4CF151FD93AbEA775D0";
+export const nftaddress = "0x03Ea715E4061a0676DF074d270d4aAE7ac98bE24";
+
+// TicketMarket Contract deployed to: 0xD542F8f9fc40d4038AEab4CF151FD93AbEA775D0
+// NFTTicket Contract deployed to: 0x03Ea715E4061a0676DF074d270d4aAE7ac98bE24
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
